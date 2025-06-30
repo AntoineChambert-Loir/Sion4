c9308615e7ccf5eda94cb0454cbd74dadc171578
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-06-30 07:59:26 -0400

    some cleaning on SemicontinuousClean

--- Sion/SubLevelset.lean
+++ Sion/SubLevelset.lean
@@ -1,45 +1,79 @@
-import Mathlib.Data.Set.Intervals.Basic
 import Mathlib.Data.Set.Defs
 import Mathlib.Data.Set.Lattice
 import Mathlib.Analysis.Convex.Topology
 import Sion.Semicontinuous
 import Sion.Concavexity
 
--- import Mathlib
-
 /-! # Sublevel sets -/
 
 open Set Function
 
-variable {α β : Type*} [TopologicalSpace α] (A : Set α) (f :α → β)
+namespace Set
+
+variable {α β : Type*}
+
+variable (A : Set α) (f : α → β) (b : β)
+
+section LeSublevel
+
+section LE
+
+variable [LE β]
+
+/-- The sublevel sets of a function -/
+def LeSublevel : Set α := { x | f x ≤ b}
+
+/-- The sublevel sets of a function -/
+def LeSublevelOn  : Set α := { x ∈ A | f x ≤ b }
+
+variable {A f b}
+
+theorem mem_leSublevel_iff {a : α} :
+  a ∈ Set.LeSublevel f b ↔ f a ≤ b := by
+  simp [LeSublevel]
+
+theorem mem_leSublevelOn_iff {a : α} :
+  a ∈ A.LeSublevelOn f b ↔ a ∈ A ∧ f a ≤ b := by
+  simp [LeSublevelOn]
+
+end LE
+
+section Preorder
 
 variable [Preorder β]
-/-- The sublevel sets of a function -/
-def Set.sublevel (b : β) : Set α := f ⁻¹' (Set.Iic b)
 
-/-- The sublevel sets of a function -/
-def Set.sublevelOn (b : β) : Set α := { x ∈ A | f x ≤ b }
-
-variable {f}
-theorem Set.mem_sublevel_iff {a : α} :
-  a ∈ Set.sublevel f b ↔ f a ≤ b := by
-  simp only [sublevel, mem_preimage, mem_Iic]
-
-theorem Set.mem_sublevelOn_iff {a : α} :
-  a ∈ A.sublevelOn f b ↔ a ∈ A ∧ f a ≤ b := by
-  simp only [sublevelOn, mem_setOf_eq]
-
-theorem Set.monotone_sublevel :
-    Monotone (fun (b : β) ↦ Set.sublevel f b) :=
+theorem monotone_sublevel :
+    Monotone (fun (b : β) ↦ LeSublevel f b) :=
   fun _ _ hbc _ hb ↦ le_trans hb hbc
 
-theorem Set.monotone_sublevelOn :
-    Monotone (fun (b : β) ↦ A.sublevelOn f b) :=
+theorem monotone_leSublevelOn :
+    Monotone (fun (b : β) ↦ A.LeSublevelOn f b) :=
   fun _ _ hbc _ ⟨ha, hb⟩ ↦ ⟨ha, le_trans hb hbc⟩
 
-theorem Set.closed_sublevel_iff :
-    (∀ b, IsClosed (Set.sublevel f b)) ↔ LowerSemicontinuous f := by
-  sorry
+end Preorder
+
+section LinearOrder
+
+variable [TopologicalSpace α] [LinearOrder β]
+
+theorem closed_LeSublevel_iff :
+    (∀ b, IsClosed (LeSublevel f b)) ↔ LowerSemicontinuous f :=
+  lowerSemicontinuous_iff_isClosed_preimage.symm
+
+theorem closed_val_preimage_LeSublevelOn_iff :
+    (∀ b, IsClosed (Subtype.val ⁻¹' (LeSublevelOn A f b) : Set A)) ↔ LowerSemicontinuousOn f A := by
+  rw [lowerSemicontinuousOn_iff_restrict,
+    ← closed_LeSublevel_iff]
+  suffices ∀ b, LeSublevel (A.restrict f) b
+    = Subtype.val ⁻¹' (A.LeSublevelOn f b) by
+      simp_rw [this]
+  intro b
+  ext ⟨x, hx⟩
+  simp [mem_leSublevel_iff, mem_leSublevelOn_iff, hx]
+
+end LinearOrder
+
+end LeSublevel
 
 /-- The overlevel sets of a function -/
 def Function.overlevel [Preorder β] (b : β) : Set α :=
